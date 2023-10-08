c5dc3b3d457ecdc72307614740285d4d9958ece9
Colin Deane <colin.w.deane@gmail.com>
2023-10-08 17:27:24 -0400

    Added use of dog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/colindme/go-course-puppy
 
 go 1.21.2
+
+require github.com/colindme/go-course-dog v0.0.0-20231008212455-e3222dd46360
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import dog "github.com/colindme/go-course-dog"
+
 func Bark() string {
 	return "Woof"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
